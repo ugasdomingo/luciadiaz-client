6015dc80165eb6b40fdc27594070ca4b0a08e78d
unknown <ugasdomingo@gmail.com>
2026-03-21 19:27:55 +0100

    adding cal urls

--- src/static/therapies.js
+++ src/static/therapies.js
@@ -7,8 +7,13 @@ export const therapies = [
         precio: 40,
         duration: '45 min',
         modalities: ['Online', 'Presencial Madrid'],
-        description: 'Un espacio seguro y confidencial para trabajar tu bienestar emocional. Te acompaño a explorar tus patrones, sanar heridas y construir la vida que deseas.',
-        highlights: ['Enfoque personalizado', 'Seguimiento continuo', 'Online o presencial'],
+        description:
+            'Un espacio seguro y confidencial para trabajar tu bienestar emocional. Te acompaño a explorar tus patrones, sanar heridas y construir la vida que deseas.',
+        highlights: [
+            'Enfoque personalizado',
+            'Seguimiento continuo',
+            'Online o presencial'
+        ],
         cal_link: 'https://cal.com/luciadiaz/terapia-individual',
         image: '/img/background-individual.webp'
     },
@@ -20,9 +25,14 @@ export const therapies = [
         precio: 50,
         duration: '60 min',
         modalities: ['Online', 'Presencial Madrid'],
-        description: 'Mejora la comunicación, gestiona los conflictos y fortalece el vínculo con tu pareja. Un espacio neutral donde ambos podéis ser escuchados.',
-        highlights: ['Para ambos miembros', 'Comunicación y vínculos', 'Online o presencial'],
-        cal_link: 'https://cal.com/luciadiaz/terapia-pareja',
+        description:
+            'Mejora la comunicación, gestiona los conflictos y fortalece el vínculo con tu pareja. Un espacio neutral donde ambos podéis ser escuchados.',
+        highlights: [
+            'Para ambos miembros',
+            'Comunicación y vínculos',
+            'Online o presencial'
+        ],
+        cal_link: 'https://cal.com/luciadiaz/terapia-parejas',
         image: '/img/background-parejas.webp'
     },
     {
@@ -33,9 +43,14 @@ export const therapies = [
         precio: 40,
         duration: '45 min',
         modalities: ['Online', 'Presencial Madrid'],
-        description: 'Acompañamiento a padres y madres para comprender y mejorar la relación con sus hijos. Estrategias concretas adaptadas a cada etapa del desarrollo.',
-        highlights: ['Estrategias concretas', 'Todas las edades', 'Online o presencial'],
-        cal_link: 'https://cal.com/luciadiaz/orientacion-padres',
+        description:
+            'Acompañamiento a padres y madres para comprender y mejorar la relación con sus hijos. Estrategias concretas adaptadas a cada etapa del desarrollo.',
+        highlights: [
+            'Estrategias concretas',
+            'Todas las edades',
+            'Online o presencial'
+        ],
+        cal_link: 'https://cal.com/luciadiaz/orientacion-parental',
         image: '/img/background-padres.webp'
-    },
-]
+    }
+];
